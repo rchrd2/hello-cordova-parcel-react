1c2b2a848c983ee9d78b040d77ccb47054d349a3
Richard Caceres <me@rchrd.net>
2020-11-17 21:15:32 -0800

    Hello file saving and reading

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Setup
 npm i
 npm i -g parcel
 npm i -g cordova
+cordova plugin add cordova-plugin-file
 npm run build
 npm run dev
 ```
--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,83 @@ function renderReactDom() {
   );
 }
 
+function checkFs() {
+  console.log("checkFs");
+
+  // Read-only directory where the application is installed. (iOS, Android, BlackBerry 10, OSX, windows)
+  console.log(cordova.file.applicationDirectory);
+  // Root directory of the application's sandbox; on iOS & windows this location is read-only (but specific subdirectories [like /Documents on iOS or /localState on windows] are read-write). All data contained within is private to the app. (iOS, Android, BlackBerry 10, OSX)
+  console.log(cordova.file.applicationStorageDirectory);
+
+  // Persistent and private data storage within the application's sandbox using internal memory (on Android, if you need to use external memory, use .externalDataDirectory). On iOS, this directory is not synced with iCloud (use .syncedDataDirectory). (iOS, Android, BlackBerry 10, windows)
+  console.log(cordova.file.dataDirectory);
+}
+
+function updateFs() {
+  window.requestFileSystem(
+    LocalFileSystem.PERSISTENT,
+    0,
+    function (fs) {
+      console.log("file system open: " + fs.name);
+      fs.root.getFile(
+        "hello.json",
+        { create: true, exclusive: false },
+        function (fileEntry) {
+          console.log("fileEntry is file?" + fileEntry.isFile.toString());
+          // fileEntry.name == 'someFile.txt'
+          // fileEntry.fullPath == '/someFile.txt'
+          writeFile(fileEntry, null);
+        },
+        function onErrorCreateFile() {}
+      );
+    },
+    function onErrorLoadFs() {}
+  );
+}
+
+function displayFileData(v) {
+  console.log(v);
+  document.getElementById("debug").innerText = v;
+}
+
+function readFile(fileEntry) {
+  fileEntry.file(
+    function (file) {
+      var reader = new FileReader();
+
+      reader.onloadend = function () {
+        console.log("Successful file read: " + this.result);
+        displayFileData(fileEntry.fullPath + ": " + this.result);
+      };
+
+      reader.readAsText(file);
+    },
+    function onErrorReadFile() {}
+  );
+}
+
+function writeFile(fileEntry, dataObj) {
+  // Create a FileWriter object for our FileEntry (log.txt).
+  fileEntry.createWriter(function (fileWriter) {
+    fileWriter.onwriteend = function () {
+      console.log("Successful file write...");
+      readFile(fileEntry);
+    };
+
+    fileWriter.onerror = function (e) {
+      console.log("Failed file write: " + e.toString());
+    };
+
+    // If data object is not passed in,
+    // create a new Blob instead.
+    if (!dataObj) {
+      dataObj = new Blob([JSON.stringify({hello: "world"})], { type: "text/plain" });
+    }
+
+    fileWriter.write(dataObj);
+  });
+}
+
 if (window.cordova) {
   document.addEventListener(
     "deviceready",
@@ -27,6 +104,17 @@ if (window.cordova) {
           "Running cordova-" + cordova.platformId + "@" + cordova.version
         );
         document.getElementById("deviceready").classList.add("ready");
+        checkFs();
+        updateFs();
+
+        window.addEventListener(
+          "filePluginIsReady",
+          function () {
+            console.log("File plugin is ready");
+            checkFs();
+          },
+          false
+        );
       }
       renderReactDom();
     },
